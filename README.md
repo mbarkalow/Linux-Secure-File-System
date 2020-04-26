3daefbdd1f8016bdfc6a7304aa676fbe2356c15e
Valdet <val.krivca@gmail.com>
2020-04-26 14:54:17 -0400

    added some more functionality

--- README.md
+++ README.md
@@ -5,11 +5,24 @@ Project Description:
 
 • A protected file system
 
-• For a given folder and files inside, the system only allows the account Alice to use certain programs to 
+• For a given folder and files inside, the system only allows the account Alice to use certain programs to
   create/read/edit/delete it
-  
+
 • You need to assign correct permissions
 
 • Other accounts are not able to read the content
 
 • Purely user level file system is fine
+
+
+Usage:
+
+TO COMPILE THE PROGRAM:
+  $> make
+
+TO RUN THE PROGRAM:
+  (If run.sh doesn't have permission to execute):
+  $> chmod +x run.sh
+
+  Once permissions are given to run.sh, you only have to run below command
+  $> ./run.sh
--- SecFS.java
+++ SecFS.java
@@ -12,6 +12,13 @@ public class SecFS{
 		System.out.println("Connected.");
     System.out.println("Welcome " + user);
 		Scanner Scan = new Scanner(System.in);
+
+		//this is creating the root folder in which SecFS operates
+		//if it already exists, the program just ignores this
+		new File("SecFS").mkdir();
+		String command = "chmod 700 " + "SecFS";
+		Process process = Runtime.getRuntime().exec(command);
+
 		while(true){
 
 			System.out.print("$> "); //Prompt
@@ -25,13 +32,16 @@ public class SecFS{
 				System.out.println("**  1. list ## To get the list of files in SecFS          **");
 				System.out.println("**  2. make dir_name ## To create a directory in SecFS    **");
 				System.out.println("**  3. create file_name ## To create a file in SecFS      **");
-				System.out.println("**  4. exit ## To disconnect from SecFS                   **");
+				System.out.println("**  4. remove file_name ## To remove a file in SecFS      **");
+				System.out.println("**  5. write file_name ## To write in a file in SecFS     **");
+				System.out.println("**  6. read file_name ## To read a file in SecFS     			**");
+				System.out.println("**  7. exit ## To disconnect from SecFS                   **");
 				System.out.println("************************************************************");
 
 			} else if(Split_Commands[0].equals("list")){
-        System.out.println("Listing Directories and Files in SecFS...");
-				System.out.println("************************************************************");
-				File curDir = new File(".");
+        // System.out.println("Listing Directories and Files in SecFS...");
+				// System.out.println("************************************************************");
+				File curDir = new File("SecFS/");
 				File[] filesList = curDir.listFiles();
         for(File f : filesList){
           if(f.isDirectory())
@@ -40,26 +50,44 @@ public class SecFS{
             System.out.println("--" + f.getName());
           }
         }
-      } else if(Split_Commands[0].equals("make")){
-				String dirName = Split_Commands[1];
+      } else if(Split_Commands[0].equals("make")){ //To create a directory in SecFS
+				String dirName = "SecFS/" + Split_Commands[1];
+				String comm = "chmod 700 " + dirName;
         System.out.println("Making directory in SecFS...");
 				new File(dirName).mkdir();
+				Process proc = Runtime.getRuntime().exec(comm);
 				System.out.println("Made directory " + dirName + " in SecFS...");
-
-      } else if(Split_Commands[0].equals("create")){
-				String fname = Split_Commands[1];
-				String command = "chmod 700 " + fname;
-				System.out.println("COMMAND: " + command);
+      } else if(Split_Commands[0].equals("create")){ //To create a file in SecFS
+				String fname = "SecFS/" + Split_Commands[1];
+				String comm = "chmod 700 " + fname;
         System.out.println("Creating file in SecFS...");
 				File file = new File(fname);
 				if(file.createNewFile()){
-					Process process = Runtime.getRuntime().exec(command);
+					Process proc = Runtime.getRuntime().exec(comm);
           System.out.println("Created file " + fname + " in SecFS...");
         }else {
 					System.out.println("File already exists");
 				}
-
-      }else if(Split_Commands[0].equals("exit")){
+      }else if(Split_Commands[0].equals("remove")){ //To remove a file in SecFS
+				String fname = "SecFS/" + Split_Commands[1];
+				String comm = "rm " + fname;
+				Process proc = Runtime.getRuntime().exec(comm);
+        System.out.println("Removed file " + fname + " from SecFS...");
+      }else if(Split_Commands[0].equals("write")){ //To write in a file in SecFS
+				//THIS DOESNT WORK YET. CAN USE NOTEPAD METHOD PROVIDED BY JON
+				// String fname = "SecFS/" + Split_Commands[1];
+				// String comm = "vi " + fname;
+				// Process proc = Runtime.getRuntime().exec(comm);
+      }else if(Split_Commands[0].equals("read")){ //To read a file in SecFS
+				String fname = "SecFS/" + Split_Commands[1];
+				String comm = "cat " + fname;
+				Process proc = Runtime.getRuntime().exec(comm);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line = "";
+        while((line = reader.readLine()) != null) {
+          System.out.print(line + "\n");
+        }
+      }else if(Split_Commands[0].equals("exit")){ //To disconnect from SecFS
         System.out.println("Exiting SecFS console now...");
         System.exit(0);
       }
